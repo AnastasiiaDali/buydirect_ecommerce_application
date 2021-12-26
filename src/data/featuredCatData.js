bde5fc1449a00e42d01341de134c96bd1256968c
AnastasiiaDali <anastasiia.dalakishvili@gmail.com>
2021-12-26 14:02:47 +0000

    added data

--- src/data/featuredCatData.js
+++ src/data/featuredCatData.js
@@ -6,16 +6,19 @@ export const featuredCatData = [
   {
     title: 'Men | Shop New Items',
     image: featuredCatMenPic,
-    description: 'Women | Shop New Items'
+    description: 'Women | Shop New Items',
+    url: "men's%20clothing"
   },
   {
     title: 'Women | Shop New Items',
     image: featuredCatWomenPic,
-    description: 'Women | Shop New Items'
+    description: 'Women | Shop New Items',
+    url: "women's%20clothing"
   },
   {
     title: 'Jewellery | New Collection',
     image: featuredCatJewerellyPic,
-    description: 'Women | Shop New Items'
+    description: 'Women | Shop New Items',
+    url: 'jewelery'
   }
 ];
