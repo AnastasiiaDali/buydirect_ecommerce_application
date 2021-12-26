2aa8eeb300c54b431b9db2c92d85f81812eebda2
AnastasiiaDali <anastasiia.dalakishvili@gmail.com>
2021-12-26 14:01:05 +0000

    add footer links and made it responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,22 +2,37 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Box, Grid, Typography } from '@material-ui/core';
 import { LightGreyBg } from 'theme';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
-import TwitterIcon from '@material-ui/icons/Twitter';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-// import { Link } from 'react-router-dom';
+import { socialMediaData } from 'data/socialMediaData';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   footerContainer: {
     maxWidth: '100vw',
     padding: '20px 40px 0',
     backgroundColor: LightGreyBg,
     margin: '20px 0 0 0',
-    fontSize: '16px'
+    fontSize: '16px',
+    display: 'flex',
+    [theme.breakpoints.down('xs')]: {
+      padding: 16
+    }
   },
-  '@media(minWidth: 780px)': {
-    width: '80%'
+  footerContainerBox: {
+    justifyContent: 'center',
+    [theme.breakpoints.up('md')]: {
+      maxWidth: 1280,
+      margin: '0 auto'
+    }
+  },
+  socialMedia: {
+    justifyContent: 'space-between',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      flexDirection: 'column'
+    }
+  },
+  gridItem: {
+    display: 'flex',
+    gridGap: '10px'
   }
 }));
 
@@ -26,8 +41,22 @@ export default function Footer() {
 
   return (
     <Box className={classes.footerContainer}>
-      <Grid container spacing={3}>
-        <Grid container item xs={6} spacing={1} direction="column" alignItems="flex-start">
+      <Grid container spacing={3} className={classes.footerContainerBox}>
+        <Grid container item xs={4} md={3} spacing={1} direction="column" alignItems="flex-start">
+          <Grid item>
+            <Typography xs={1}>CATEGORIES</Typography>
+          </Grid>
+          <Grid item>
+            <Typography xs={1}>Women Clothes</Typography>
+          </Grid>
+          <Grid item>
+            <Typography xs={1}>Men Clothes</Typography>
+          </Grid>
+          <Grid item>
+            <Typography xs={1}>Jewerely</Typography>
+          </Grid>
+        </Grid>
+        <Grid container item xs={4} md={3} spacing={1} direction="column" alignItems="flex-start">
           <Grid item>
             <Typography xs={1}>ACCOUNT</Typography>
           </Grid>
@@ -41,7 +70,7 @@ export default function Footer() {
             <Typography xs={1}>Orders</Typography>
           </Grid>
         </Grid>
-        <Grid container item xs={6} spacing={1} direction="column">
+        <Grid container item xs={4} md={3} spacing={1} direction="column">
           <Grid item>
             <Typography>ABOUT US</Typography>
           </Grid>
@@ -52,32 +81,22 @@ export default function Footer() {
             <Typography>FAQs</Typography>
           </Grid>
         </Grid>
-        <Grid container item xs={12} justifyContent="space-between">
+        <Grid className={classes.socialMedia} container item xs={12} md={3}>
           <Grid item>
-            <Typography>SOCIAL MEDIA</Typography>
+            <Typography paragraph>SOCIAL MEDIA</Typography>
           </Grid>
-          <Grid item>
-            {/* <Link to={{ pathname: 'https://www.instagram.com/' }} target="_blank"> */}
-            <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-              <InstagramIcon />
-            </a>
-            {/* </Link> */}
-          </Grid>
-          <Grid item>
-            <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-              <FacebookIcon />
-            </a>
-          </Grid>
-          <Grid item>
-            <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
-              <TwitterIcon />
-            </a>
-          </Grid>
-          <Grid item>
-            <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
-              <LinkedInIcon />
-            </a>
-          </Grid>
+          {socialMediaData.map((social) => {
+            return (
+              <Grid item key={social.id} className={classes.gridItem}>
+                <a href={social.link} target="_blank" rel="noreferrer">
+                  {social.icon}
+                </a>
+                <Box display={{ xs: 'none', md: 'flex' }}>
+                  <Typography>{social.label}</Typography>
+                </Box>
+              </Grid>
+            );
+          })}
         </Grid>
       </Grid>
     </Box>
