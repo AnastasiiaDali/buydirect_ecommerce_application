407961a964eaa2a503b3d4bb11a2b48191f81397
AnastasiiaDali <anastasiia.dalakishvili@gmail.com>
2021-12-28 18:55:28 +0000

    add discounted items category

---
+++ src/components/DiscountedItems/DiscountedItems.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Skeleton from '@material-ui/lab/Skeleton';
+import { Typography, Button, FormControlLabel, Checkbox } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import Favorite from '@material-ui/icons/Favorite';
+import { PrimaryBlue } from 'theme';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { useDispatch } from 'react-redux';
+import { addToCart } from 'features/cart/cartSlice';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: '0px 16px 0',
+    maxWidth: '1280px',
+    margin: '0 auto',
+    [theme.breakpoints.down('sm')]: {
+      padding: '0px 8px 0'
+    }
+  },
+  topSellersContainer: {
+    display: 'flex',
+    overflow: 'scroll',
+    margin: '40px 0'
+  },
+  imageProduct: {
+    objectFit: 'scale-down',
+    width: '190px',
+    height: '190px',
+    margin: '5px',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  productContainer: {
+    display: 'grid',
+    minWidth: '200px',
+    height: 'auto',
+    border: `1px solid ${PrimaryBlue}`,
+    borderRadius: '5px',
+    margin: '5px',
+    backgroundColor: '#fff'
+  },
+
+  productTitle: {
+    display: '-webkit-box',
+    lineClamp: 2,
+    minHeight: '52px',
+    boxOrient: 'vertical',
+    overflow: 'hidden',
+    margin: '10px'
+  },
+  productPrice: {
+    padding: '2px',
+    margin: 'auto 10px 10px 10px'
+  },
+  addBtnFav: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '4px'
+  },
+  favorite: {
+    margin: '0px'
+  }
+}));
+
+export default function DiscountedItems() {
+  const classes = useStyles();
+  const { data: products, isLoading } = useQuery('products', () =>
+    fetch('https://fakestoreapi.com/products').then((res) => res.json())
+  );
+
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
+
+  return (
+    <Box className={classes.container}>
+      <Typography variant="h2" align="center" style={{ fontFamily: 'Montserrat' }}>
+        On Sale
+      </Typography>
+
+      <Box className={classes.topSellersContainer}>
+        {isLoading &&
+          Array.from(Array(10).keys()).map((i) => (
+            <Box className={classes.productContainer} key={i}>
+              <Skeleton variant="rect" className={classes.imageProduct} />
+              <Skeleton variant="text" className={classes.productTitle} />
+              <Box display="flex" gridGap="8px" justifyContent="center" alignItems="center">
+                <Skeleton variant="rect" width={130} height={30} />
+                <Skeleton variant="circle" width={40} height={40} />
+              </Box>
+            </Box>
+          ))}
+        {products?.map((product) => {
+          const { id, title, price, image } = product;
+
+          return (
+            <Box key={id} className={classes.productContainer}>
+              <Box component={Link} to={`/products/${id}`}>
+                <Box
+                  className={classes.imageProduct}
+                  style={{ background: `url(${image}) center center/contain no-repeat` }}
+                />
+                <Typography variant="h4" className={classes.productTitle}>
+                  {title}
+                </Typography>
+                <Typography variant="subtitle1" className={classes.productPrice}>
+                  ${price}
+                </Typography>
+                <Typography variant="h4" className={classes.productPrice}>
+                  {`${(price * 0.7).toFixed(2)}`}
+                </Typography>
+              </Box>
+              <div className={classes.addBtnFav}>
+                <Button variant="contained" onClick={() => handleAddToCart(product)}>
+                  ADD TO CART
+                </Button>
+                <FormControlLabel
+                  className={classes.favorite}
+                  control={
+                    <Checkbox
+                      icon={<FavoriteBorder />}
+                      checkedIcon={<Favorite color="primary" />}
+                    />
+                  }
+                />
+              </div>
+            </Box>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,6 +5,7 @@ import Hero from 'components/Hero/Hero';
 import FeaturedCat from 'components/FeaturedCat/FeaturedCat';
 import TopSellers from 'components/TopSellers/TopSellers';
 import SpecialSale from 'components/SpecialSale/SpecialSale';
+import DiscountedItems from 'components/DiscountedItems/DiscountedItems';
 
 function MainPage() {
   return (
@@ -13,6 +14,7 @@ function MainPage() {
       <SideBar />
       <Hero />
       <FeaturedCat />
+      <DiscountedItems />
       <SpecialSale />
       <TopSellers />
     </>
