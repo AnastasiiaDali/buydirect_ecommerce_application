e15143077b8ab4cab0146c92f566e339f823160d
AnastasiiaDali <anastasiia.dalakishvili@gmail.com>
2021-12-28 18:59:33 +0000

    add oe more address

--- src/data/accountData.js
+++ src/data/accountData.js
@@ -3,5 +3,6 @@ export const accountData = {
   name: 'Johny Black',
   billingAddress: 'USA, NY, 5th Avenue, building 10',
   shippingAddress: 'UK, London, 221B Baker Street ',
+  secondaryShippingAddress: 'UK, London, 111 Green Street ',
   storeCredit: '$23.78'
 };
