b12ed0701c580c140d57551a1e5facfe0956af9e
AnastasiiaDali <anastasiia.dalakishvili@gmail.com>
2021-12-28 12:39:50 +0000

    add OurstoryPage and FaqPage routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import CartPage from 'pages/CartPage';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
 import AccountPage from 'pages/AccountPage';
+import FaqPage from 'pages/FaqPage';
+import OurStoryPage from 'pages/OurStoryPage';
 
 function App() {
   return (
@@ -19,6 +21,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/account" element={<AccountPage />} />
+        <Route path="/faq" element={<FaqPage />} />
+        <Route path="/ourstory" element={<OurStoryPage />} />
       </Routes>
     </div>
   );
